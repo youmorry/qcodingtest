99514279a1cd3cf29ce0991ad60d5270ce88fbc2
yumori <youmorry@gmail.com>
2026-06-27 05:41:22 +0000

    feat: init springboot

---
+++ src/main/kotlin/com/example/qcodingtest/QcodingtestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.qcodingtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QcodingtestApplication
+
+fun main(args: Array<String>) {
+	runApplication<QcodingtestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/qcodingtest/QcodingtestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.qcodingtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QcodingtestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
