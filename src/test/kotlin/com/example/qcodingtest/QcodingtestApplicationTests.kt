445b2024d3f9b563c29f814dd705359d17ad337c
yumori <youmorry@gmail.com>
2026-07-01 14:36:15 +0000

    refactor: 不要なテストを削除

--- src/test/kotlin/com/example/qcodingtest/QcodingtestApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.example.qcodingtest
-
-import org.junit.jupiter.api.Test
-
-class QcodingtestApplicationTests : AbstractIntegrationTest() {
-    @Test
-    fun contextLoads() {
-    }
-}
